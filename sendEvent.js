2f9628a91ba52e2450203d997f8781912ff61cb7
Ed Sweeney <esweeney@starbucks.com>
2018-05-04 07:46:22 -0700

    func

--- sendEvent.js
+++ sendEvent.js
@@ -4,15 +4,21 @@ const { EventHubClient, EventPosition } = require('azure-event-hubs');
 
 const client = EventHubClient.createFromConnectionString(process.env["EVENTHUB_CONNECTION_STRING"], process.env["EVENTHUB_NAME"]);
 
-async function main() {
-  const eventData = { body: "Hello World", partitionKey: "pk12345"};
+const DATA = '{"msg":"hi", "id": 123}';
+
+async function send(data, key) {
+  console.log("sending key " + key)
+  const eventData = { body: data, partitionKey: key};
   //const delivery = await client.send(eventData);
   const delivery = await client.send(eventData,1);
   console.log("message sent successfully.");
   process.exit(1)
 }
 
-main().catch((err) => {
+const parsedData = JSON.parse(DATA);
+const key = parsedData.id;
+
+send(DATA, key).catch((err) => {
   console.log(err);
 });
 
